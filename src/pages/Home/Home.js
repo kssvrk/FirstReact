31ebbc66f9726a07d4ddbca7a01d6722051f4403
kssvrk <kssvrk@gmail.com>
2021-02-07 23:26:14 +0530

    added user hello

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,20 @@
-function Home() {
+import { useState, useEffect } from "react";
+import { WhichUser } from "../../services/AuthToken";
+
+const Home = () => {
+  const [user, setUser] = useState("");
+  let edit = false;
+  let callback = (data) => {
+    setUser(data.username);
+  };
+  if (user === "") WhichUser(setUser);
+
+  //console.log(user);
   return (
     <div>
-      <h1>Welcome to Aikops</h1>
+      <h1>Welcome to Aikops {user.email}</h1>
     </div>
   );
-}
+};
 
 export default Home;
--- src/services/AuthToken.js
+++ src/services/AuthToken.js
@@ -1,6 +1,32 @@
 import { useHistory, Redirect } from "react-router-dom";
 import axios from "axios";
 
+// ------------- SAMPLE -----------------
+
+// axios
+//     .post(user_url, {}, config)
+//     .then((res) => {})
+//     .catch((err) => {
+//       if (err.response) {
+//         console.log("Backend error response");
+//         console.log(error.response.data);
+//         console.log(error.response.status);
+//         console.log(error.response.headers);
+//       } else if (err.request) {
+//         alert(
+//           "Unable to make the request: Likely issue is your internet connection"
+//         );
+//       } else {
+//         alert("Something went wrong, please try later");
+//       }
+//     })
+//     .then(() => {
+//       window.localStorage.removeItem("AuthToken");
+//       setLogin(false);
+//     });
+
+//---------------------------------------
+
 export function LoggedIn() {
   console.log(window.localStorage.getItem("AuthToken"));
   if (window.localStorage.getItem("AuthToken") === null) {
@@ -8,6 +34,41 @@ export function LoggedIn() {
   } else {
     return true;
   }
+}
+
+export async function WhichUser(callback) {
+  const user_url = "http://127.0.0.1:8000/auth/users/me/";
+  var token = window.localStorage.getItem("AuthToken");
+  let history = useHistory();
+  let config = {
+    headers: {
+      Authorization: "Token " + token,
+    },
+  };
+  console.log("hello");
+
+  axios
+    .get(user_url, config)
+    .then((res) => {
+      //console.log(res.data);
+      callback(res.data);
+    })
+    .catch((err) => {
+      if (err.response) {
+        console.log("Backend error response");
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+        if (err.response.status == 401) history.push("/Logout");
+      } else if (err.request) {
+        alert(
+          "Unable to make the request: Likely issue is your internet connection"
+        );
+      } else {
+        alert("Something went wrong, please try later");
+      }
+    })
+    .then(() => {});
 }
 
 export const LogOut = (props) => {
